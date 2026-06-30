2ad524b69d260a80852a8e2d2b175396b3f4a9b9
ngaaia <ngaaia.toabwa@gmail.com>
2026-06-30 16:21:26 +1200

    enable certificate to work

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
+
